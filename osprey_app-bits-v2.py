27457fbd75805e8d991bb895d699ccd03be7aa1a
Nathaniel Brown <darksun@mbpdarksunpurefamecom.local>
2022-03-28 12:36:09 -0700

    Updating licence to CC-A-SA 3.0

--- osprey_app-bits-v2.py
+++ osprey_app-bits-v2.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3.8
 
-# MIT License
-#
+# LICENSE CREATIVE_COMMONS-ATTRIBUTION_SHARE_ALIKE
+# https://creativecommons.org/licenses/by-sa/3.0/legalcode
 # Copyright (c) 2020-2022 Nathaniel Steven Henry Brown [0+nate@purefame.com]
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 import re
 import subprocess
@@ -41,30 +23,10 @@ if len(sys.argv) == 1:
 	motd_help = """
 Welcome to OSPREY.app's {0}
 -----------------------------------------------------
-Created by: Nathaniel Steven Henry Brown
-Email: nate@purefame.com
-
-MIT License
-
+LICENSE CREATIVE_COMMONS-ATTRIBUTION_SHARE_ALIKE
+https://creativecommons.org/licenses/by-sa/3.0/legalcode
 Copyright (c) 2020-2022 Nathaniel Steven Henry Brown [0+nate@purefame.com]
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 
 [REQS] 	This is script is compiled to run on Ubuntu 16LTS and above with 
 		python3.
